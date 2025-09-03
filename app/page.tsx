5cb51610887457195db64b05e80755e4d8740b47
Astra <abhaypratapsinghwd@gmail.com>
2025-09-03 11:12:47 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className="min-h-screen flex flex-col gap-4 items-center justify-center">
+      <h2 className="text-3xl my-8">Generic Landing Page</h2>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ config/site.ts
@@ -0,0 +1,12 @@
+import { SiteConfig } from "@/interfaces/site";
+
+export const siteConfig: SiteConfig = {
+  name: "LandingX",
+  description: "Generic Landing page",
+  url: "https://demo-motionx.vercel.app",
+  links: {
+    twitter: "https://x.com/XSinghAstra",
+    github: "https://github.com/SinghAstra",
+    githubRepo: "https://github.com/SinghAstra/LandingX",
+  },
+};
