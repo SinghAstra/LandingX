f55e427546174388a2f56390e879e4413215d7d0
Astra <abhaypratapsinghwd@gmail.com>
2025-09-19 16:29:55 +0530

    Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
+import Navbar from "@/components/navigation/navbar";
 import React from "react";
 
 const HomePage = () => {
   return (
-    <div className="min-h-screen flex flex-col gap-4 items-center justify-center">
-      <h2 className="text-3xl my-8">Generic Landing Page</h2>
+    <div className="min-h-screen ">
+      <Navbar />
+      <div className="min-h-screen flex items-center justify-center">
+        <h1 className="text-6xl font-bold">Hey</h1>
+      </div>
+      <div className="min-h-screen flex items-center justify-center">
+        <h1 className="text-6xl font-bold">There</h1>
+      </div>
+      <div className="min-h-screen flex items-center justify-center">
+        <h1 className="text-6xl font-bold">How </h1>
+      </div>
+      <div className="min-h-screen flex items-center justify-center">
+        <h1 className="text-6xl font-bold">Are</h1>
+      </div>
+      <div className="min-h-screen flex items-center justify-center">
+        <h1 className="text-6xl font-bold">You ?</h1>
+      </div>
     </div>
   );
 };
